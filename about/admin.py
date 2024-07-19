252c08f6b40d484ffe6334ad24e632a0893d5cc7
Chris Niblock <nibbo1984@hotmail.co.uk>
2024-07-19 13:02:38 +0000

    remove about/admin.py code

--- about/admin.py
+++ about/admin.py
@@ -1,4 +1,1 @@
 from django.contrib import admin
-from .models import About
-
-admin.site.register(About)
--- history/admin.py
+++ history/admin.py
@@ -1,4 +1,1 @@
 from django.contrib import admin
-from .models import History
-
-admin.site.register(History)
