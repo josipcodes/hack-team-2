7d1bbe6348b5069ad45eb2318ae9714e73cf371e
Kayla S <kaylaesmith1@gmail.com>
2024-07-20 01:53:23 +0000

    Link up About page with base.html

--- about/views.py
+++ about/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+# def about(request):
+#     return HttpResponse("Hello, Prideful Pixels!")
+
 def about(request):
-    return HttpResponse("Hello, Prideful Pixels!")
+    return render(request, 'about.html')
