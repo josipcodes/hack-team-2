fb79e1ef2c42cd4ea8550f224bc59afebde2c30e
Chris Niblock <nibbo1984@hotmail.co.uk>
2024-07-21 10:43:56 +0000

    change history object order to order_by year

--- history/views.py
+++ history/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import History
 
 def history(request):
-    history_events = History.objects.all()
+    history_events = History.objects.all().order_by('year')
     return render(request, 'history/history.html', {'history_events': history_events})
